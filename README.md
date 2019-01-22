8a6dc804fe7fc1900d53d2c6f21612a378b4c204
Santiago Cárdenas <santicar1017@gmail.com>
2019-01-22 14:27:46 -0500

    Updated intents

--- README.md
+++ README.md
@@ -4,6 +4,21 @@ This plugins give you android PowerManager methods with cordova.
 
 To use this plugin, be sure that your app is allowed to use this features in this link: https://developer.android.com/training/monitoring-device-state/doze-standby
 
+Android Custom Roms made sometimes your apps unfunctional due to:
+* Your App is killed when it's not in foreground
+* Notification message do not appear
+* Your services is killed by agressive power saving mode
+
+### Please if you have a Samsung, Meizu, Letv test this plugin to help me, thanks!
+
+## Current protected apps supported devices:
+
+* Huawei (<span style="color:green">TESTED</span>)
+* Xiaomi (<span style="color:green">TESTED</span>)
+* Samsung (<span style="color:red">NOT TESTED</span>)
+* Meizu (<span style="color:red"> NOT TESTED</span>)
+* Letv (<span style="color:red">NOT TESTED</span>)
+
 # Instalation
 
 ```bash
@@ -72,9 +87,21 @@ cordova.plugins.PowerOptimization.ProtectedAppCheck().then((result) => {
   console.error(result);
 });
 ```
-
 # Notes
 Be sure that your AndroidManifest.xml contains this permission 
 ``` xml
 <uses-permission android:name="android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS" />
 ```
+
+## Based in this repo:
+
+[AppKillerManager](https://github.com/thelittlefireman/AppKillerManager)
+
+## Help liks:
+[backgroundable-android](https://github.com/dirkam/backgroundable-android)
+
+[Network access in doze mode](https://stackoverflow.com/questions/32316491/network-access-in-doze-mode/32424117#32424117)
+
+[REQUEST_IGNORE_BATTERY_OPTIMIZATIONS how to do it right](https://stackoverflow.com/questions/44862176/request-ignore-battery-optimizations-how-to-do-it-right)
+
+[Android PowerManager](https://developer.android.com/reference/android/os/PowerManager)
--- src/android/Constants.java
+++ src/android/Constants.java
@@ -1,19 +1,131 @@
 package cordova.plugin.PowerOptimization;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.annotation.NonNull;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
 
-public static List<Intent> POWERMANAGER_INTENTS = Arrays.asList(
-        // Xiaomi Battery usage monitoring and Power settings
-        new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.powercenter.PowerSettings")),
-        // Samsung Unmonitored Apps (7.0+)
-        new Intent().setComponent(new ComponentName("com.samsung.android.lool","com.samsung.android.sm.ui.battery.BatteryActivity")),
-        // Huawei Protected Apps (EMUI < 5)
-        new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity"))
+    private Context context;
+
+    // MIUI
+    private static final String MIUI_ACTION_POWER_SAVE_EXTRA_NAME = "package_name";
+    private static final String MIUI_ACTION_POWER_SAVE_EXTRA_LABEL = "package_label";
+    private static final String[] MIUI_ACTION_POWERSAVE = {"com.miui.powerkeeper", "com.miui.powerkeeper.ui.HiddenAppsConfigActivity"};
+
+    // SAMSUNG crash "com.samsung.android.lool","com.samsung.android.sm.ui.battery.AppSleepListActivity"
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_ACTION = "com.samsung.android.sm.ACTION_BATTERY";
+    private static final String SAMSUNG_SYSTEMMANAGER_NOTIFICATION_ACTION = "com.samsung.android.sm.ACTION_SM_NOTIFICATION_SETTING";
+    // SAMSUNG ANDROID 7.0
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V3 = "com.samsung.android.lool";
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V3_ACTIVITY = "com.samsung.android.sm.ui.battery.BatteryActivity";
+
+    // SAMSUNG ANDROID 6.0
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V2 = "com.samsung.android.sm_cn";
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V2_ACTIVITY = "com.samsung.android.sm.ui.battery.BatteryActivity";
+
+    // SAMSUNG ANDROID 5.0/5.1
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V1 = "com.samsung.android.sm";
+    private static final String SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V1_ACTIVITY = "com.samsung.android.sm.ui.battery.BatteryActivity";
+
+    // HUAWEI
+    private static final String HUAWEI_ACTION_POWERSAVING = "huawei.intent.action.HSM_PROTECTED_APPS";
+
+    // LETV
+    private static final String LETV_ACTION_POWERSAVING_PKG = "com.letv.android.letvsafe";
+    private static final String LETV_ACTION_POWERSAVING_CLS = "com.letv.android.letvsafe.BackgroundAppManageActivity";
+
+    // MEIZU
+
+    private static final String MEIZU_POWERSAVING_ACTION = "com.meizu.power.PowerAppKilledNotification";
+    private static final String MEIZU_DEFAULT_PACKAGE = "com.meizu.safe";
+    private static final String MEIZU_POWERSAVING_ACTIVITY_V2_2 = "com.meizu.safe.cleaner.RubbishCleanMainActivity";
+    private static final String MEIZU_POWERSAVING_ACTIVITY_V3_4 = "com.meizu.safe.powerui.AppPowerManagerActivity";
+    private static final String MEIZU_POWERSAVING_ACTIVITY_V3_7 = "com.meizu.safe.powerui.PowerAppPermissionActivity";
+
+    public List<Intent> powerManagerIntents;
+
+    public Constants(Context context){
+        populateList();
+    }
+
+
+    private void populateList(){
+
+        // Xiaomi intents
+        Intent xiomi_1 = new Intent();
+        xiomi_1.setComponent(new ComponentName(MIUI_ACTION_POWERSAVE[0], MIUI_ACTION_POWERSAVE[1]));
+        xiomi_1.putExtra(MIUI_ACTION_POWER_SAVE_EXTRA_NAME, context.getPackageName());
+        xiomi_1.putExtra(MIUI_ACTION_POWER_SAVE_EXTRA_LABEL, getApplicationName());
+
+        // Samsung intents
+        // TODO Test it
+        Intent samsung_1 = new Intent();
+        samsung_1.setAction(SAMSUNG_SYSTEMMANAGER_POWERSAVING_ACTION);
+        Intent samsung_2 = new Intent();
+        samsung_2.setComponent(new ComponentName(SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V3, SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V3_ACTIVITY));
+        Intent samsung_3 = new Intent();
+        samsung_3.setComponent(new ComponentName(SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V2, SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V2_ACTIVITY));
+        Intent samsung_4 = new Intent();
+        samsung_4.setComponent(new ComponentName(SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V1, SAMSUNG_SYSTEMMANAGER_POWERSAVING_PACKAGE_V1_ACTIVITY));
+
+        // Huawei
+        Intent huawei_1 = new Intent();
+        huawei_1.setAction(HUAWEI_ACTION_POWERSAVING);
+
+        // LeTV
+        // TODO Test it
+        Intent letv_1 = new Intent();
+        huawei_1.setComponent(new ComponentName(LETV_ACTION_POWERSAVING_PKG, LETV_ACTION_POWERSAVING_CLS));
+
+        // Meizu
+        // TODO Test it
+        Intent meizu_1 = new Intent();
+        meizu_1.setAction(MEIZU_POWERSAVING_ACTION);
+        Intent meizu_2 = new Intent();
+        meizu_2.setClassName(MEIZU_DEFAULT_PACKAGE, MEIZU_POWERSAVING_ACTIVITY_V2_2);
+        Intent meizu_3 = new Intent();
+        meizu_3.setClassName(MEIZU_DEFAULT_PACKAGE, MEIZU_POWERSAVING_ACTIVITY_V3_4);
+        Intent meizu_4 = new Intent();
+        meizu_4.setClassName(MEIZU_DEFAULT_PACKAGE, MEIZU_POWERSAVING_ACTIVITY_V3_7);
+
         // TODO ASUS, OPPO, Elephone, Sony STAMINA, Huawei Phone manager (EMUI 5)
-);
+
+        powerManagerIntents = Arrays.asList(
+                xiomi_1,
+                samsung_1,
+                samsung_2,
+                samsung_3,
+                samsung_4,
+                huawei_1,
+                letv_1,
+                meizu_1,
+                meizu_2,
+                meizu_3,
+                meizu_4
+        );
+    }
+
+    public List<Intent> getPowermanagerIntents(){
+        return powerManagerIntents;
+    }
+
+
+    public String getApplicationName() {
+        PackageManager packageManager = context.getPackageManager();
+        ApplicationInfo applicationInfo = null;
+        try {
+            applicationInfo = packageManager.getApplicationInfo(context.getApplicationInfo().packageName, 0);
+        } catch (final PackageManager.NameNotFoundException e) {
+        }
+        return (String) (applicationInfo != null ? packageManager.getApplicationLabel(applicationInfo) : "Unknown");
+    }
+
 }
